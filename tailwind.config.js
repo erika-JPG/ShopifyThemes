3093e3dadb788e6fe39a2b126e83c258fa8d4a4b
Erika J Ponce <erika.js.ponce@gmail.com>
2024-02-13 07:46:41 -0800

    Navigation bar

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './layout/*.liquid',
+    './sections/*.liquid',
+    './snippets/*.liquid',
+    './templaes/customers/*.liquid',
+    './templates/*.liquid'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
